82085ecdba33526285a9271a8e36d177dd01fea2
Shenhaoyi <shenhaoyi04@163.com>
2023-09-15 02:05:05 +0800

    添加打包指令

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import subtract from './/subtract';
-import sum from './sum';
+import subtract from './subtract.js';
+import sum from './sum.js';
 
 console.log(subtract(2, 1));
 console.log(sum(1, 2, 3, 4));
