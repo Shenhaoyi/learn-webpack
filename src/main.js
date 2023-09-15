ac6c740934f51d3662d38ccee528c4b1812b1d40
Shenhaoyi <shenhaoyi04@163.com>
2023-09-16 00:46:45 +0800

    修改文件路径

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import subtract from './subtract.js';
-import sum from './sum.js';
+import subtract from './js/subtract.js';
+import sum from './js/sum.js';
 
 console.log(subtract(2, 1));
 console.log(sum(1, 2, 3, 4));
