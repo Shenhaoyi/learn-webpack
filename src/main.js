99c061b7e487b171b3841ac9847036efb9f7707f
Shenhaoyi <shenhaoyi04@163.com>
2023-09-17 18:23:35 +0800

    iconfont3种使用方式

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import subtract from './js/subtract.js';
 import sum from './js/sum.js';
 import './css/index.css';
 import './less/index.less';
+import './assets/iconfont/iconfont.css'; // font class 和unicode
+import './assets/iconfont/iconfont.js';
 
 console.log(subtract(2, 1));
 console.log(sum(1, 2, 3, 4));
