a6229812bbc5ee075f6252ab7ba6b0467ac96658
Shenhaoyi <shenhaoyi04@163.com>
2023-09-24 19:22:16 +0800

    安装并配置devServer

--- webpack.config.js
+++ webpack.config.js
@@ -67,6 +67,12 @@ export default {
       template: path.resolve(process.cwd(), 'public/index.html'),
     }),
   ],
+  // 开发服务器
+  devServer: {
+    host: 'localhost', // 启动服务器域名
+    port: '3000', // 启动服务器端口号
+    open: true, // 启动后自动打开默认浏览器
+  },
   // 模式
   mode: 'development',
 };
