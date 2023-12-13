248f938d9bbaf9b5c469332a31f253e2af4c4801
Shenhaoyi <shenhaoyi04@163.com>
2023-12-14 00:29:29 +0800

    路由懒加载

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
+
+const Lazy = lazy(() => import(/* webpackChunkName: "lazy" */ './pages/Lazy'));
 
 export default function App() {
   return (
@@ -9,11 +11,16 @@ export default function App() {
       <NavLink to="/home">home</NavLink>
       &nbsp;
       <NavLink to="/about">about</NavLink>
-      <Switch>
-        <Route path="/about" component={About} />
-        <Route path="/home" component={Home} />
-        <Redirect to="/home" />
-      </Switch>
+      &nbsp;
+      <NavLink to="/lazy">lazy</NavLink>
+      <Suspense fallback={<div>loading...</div>}>
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/home" component={Home} />
+          <Route path="/lazy" component={Lazy} />
+          <Redirect to="/home" />
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   );
 }
---
+++ src/pages/Lazy/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Lazy() {
+  return (
+    <div>
+      <h1>lazy</h1>
+    </div>
+  );
+}
