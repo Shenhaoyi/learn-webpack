0ff9c0436aec8d704663fa7522c87c8d73b3c80a
Shenhaoyi <shenhaoyi04@163.com>
2023-12-13 16:43:09 +0800

    使用 history router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import Counter from './components/Counter';
+import { BrowserRouter, Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+
 export default function App() {
   return (
-    <div>
-      <h1>hello world</h1>
-      <Counter />
-    </div>
+    <BrowserRouter>
+      <NavLink to="/home">home</NavLink>
+      &nbsp;
+      <NavLink to="/about">about</NavLink>
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/home" component={Home} />
+        <Redirect to="/home" />
+      </Switch>
+    </BrowserRouter>
   );
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Counter from '../../components/Counter';
+export default function Home() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <Counter />
+    </div>
+  );
+}
